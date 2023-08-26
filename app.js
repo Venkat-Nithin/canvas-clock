a7349bdaa33cd612a65431a85ba6d68c6b9f0201
Venkat-Nithin <141633918+Venkat-Nithin@users.noreply.github.com>
2023-08-26 17:53:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,80 +1,69 @@
-// object for canvas
-var canvas = document.getElementById ("canvas");
+var canvas = document.getElementById ("canvas")
 
-//creating 2d object
-var context = canvas.getContext("2d");
+var context = canvas.getContext("2d")
 
-//clock radius
-var radius= canvas.height/2;
+var radius= canvas.height/2
 
-context.translate(radius,radius);
+context.translate(radius,radius)
 
-radius = radius*0.90;
+radius = radius*0.90
 
 
 function drawFace(context,radius){
-    var grad;
-    //drawing white area in the clock
-    context.beginPath();
-    context.arc(0,0,radius,0,2*Math.PI);
-    context.fillStyle="#191919";
-    context.fill();
+    var grad
+    context.beginPath()
+    context.arc(0,0,radius,0,2*Math.PI)
+    context.fillStyle="#191919"
+    context.fill()
     
-    // creating radial color
-    grad= context.createRadialGradient(0,0,radius*0.80,0,0,radius*0.70);
-    grad.addColorStop(0,"#191919");
-    grad.addColorStop(0.5,"#191919");
-    grad.addColorStop(1,"#535252");
-    context.strokeStyle=grad;
-    context.lineWidth = radius*0.1;
-    context.stroke();
+    grad= context.createRadialGradient(0,0,radius*0.80,0,0,radius*0.70)
+    grad.addColorStop(0,"#191919")
+    grad.addColorStop(0.5,"#191919")
+    grad.addColorStop(1,"#535252")
+    context.strokeStyle=grad
+    context.lineWidth = radius*0.1
+    context.stroke()
 
-    //clock center part
-    context.beginPath();
+    context.beginPath()
     context.arc(0,0,radius*0.065,0,2*Math.PI)
     context.fillStyle="#535252"
-    context.fill();
+    context.fill()
 }
 
 
 
 function drawNumber(context,radius){
     var num , ang;
-    //set the font size 
-    context.font=radius*0.17 + "px raleway";
-    //text alignment to middle and place in center position
+    context.font=radius*0.17 + "px raleway"
     context.textBaseline="middle"
     context.textAlign="center"
     for(num=1;num<13;num++){
         ang=num*Math.PI/6;
-        context.rotate(ang);
+        context.rotate(ang)
         context.translate(0,-radius*0.85)
-        context.rotate(-ang);
-        context.fillText(num.toString(),0,0);
-        context.rotate(ang);
-        context.translate(0,radius*0.85);
-        context.rotate(-ang);
+        context.rotate(-ang)
+        context.fillText(num.toString(),0,0)
+        context.rotate(ang)
+        context.translate(0,radius*0.85)
+        context.rotate(-ang)
     }
 }
 
 
 
 function drawTime(context,radius){
-    var now = new Date();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second =  now.getSeconds();
-    //calculate the angle 
-    hour=hour%12;
-    //hour hand
+    var now = new Date()
+    var hour = now.getHours()
+    var minute = now.getMinutes()
+    var second =  now.getSeconds()
+    hour=hour%12
+
     hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60))
     drawHand(context,hour,radius*0.4,radius*0.04)
 
-    //minute hand
     minute=(minute*Math.PI/30) + (second*Math.PI/(30*60))
     drawHand(context,minute,radius*0.6,radius*0.04)
 
-    //second hand
     second=(second*Math.PI/30)
     drawHand(context,second,radius*0.9,radius*0.01)
 }
@@ -95,7 +84,7 @@ function drawHand(context,pos,length,width){
 setInterval(drawClock,1000)
 
 function drawClock(){
-    drawFace(context,radius);
+    drawFace(context,radius)
     drawNumber(context,radius)
     drawTime(context,radius)
 }
